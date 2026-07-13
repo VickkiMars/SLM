4884c50fe9358a69acdc5df9294c8a854568feb3
Jarvis <jarviskrost2000@gmail.com>
2026-07-13 11:27:19 +0100

    Update auth.py
    updated everything

--- middleware/auth.py
+++ middleware/auth.py
@@ -3,11 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def verify_token(token: str):
+async def verify_token(token: str):
     payload = jwt.decode(
         token,
         os.getenv("SUPABASE_JWT_SECRET"),
         algorithms=["HS256"],
         audience="authenticated"
     )
-    return payload["sub"]
+    if payload:
+      data = {
+        "user_id": payload["sub"],
+        "user_email": payload.get("email"),
+        "user_name": payload.get("name")
+      } 
+      return data
+    else:
+      return 
