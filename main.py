61c0eb587ce754798a4d98fc06cc0b431473de3c
Jarvis <jarviskrost2000@gmail.com>
2026-07-13 11:47:05 +0100

    Update main.py
    a missed quote 😭😭

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ async def send_text(authorisation: str = Header(None)):
     details = {
       "user_id" = data["user_id"],
       "user_name" = data["user_name"],
-      "user_email = data["user_email"]
+      "user_email" = data["user_email"]
     }
     return details
   
